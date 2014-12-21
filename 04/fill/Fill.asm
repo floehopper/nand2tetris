7d46008848c4509a4467cac1787c3272ee70c6ae
James Mead <james@floehopper.org>
2014-12-21 19:07:52 +0000

    Implement Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,39 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(POLL)
+   @colour
+   M=0
+   @KBD
+   D=M
+   @WHITE
+   D;JEQ
+   @colour
+   M=M-1
+(WHITE)
+   @SCREEN
+   D=A
+   @address
+   M=D
+(LOOP)
+   @address
+   D=M
+   @24576
+   D=D-A
+   @POLL
+   D;JGE
+   @colour
+   D=M
+   @address
+   A=M
+   M=D
+   D=A+1
+   @address
+   M=D
+   @LOOP
+   0;JMP
